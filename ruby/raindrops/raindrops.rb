0dd8dc325b276a1f8b196e496e5cfb22d2dd0701
sbatsch <sbatsch2@gmail.com>
2020-02-18 16:45:44 -0600

    Raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,8 +1,21 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
-
+class Raindrops
+  def self.convert(number)
+    if number % 5 == 0 && number % 7 == 00 && number % 3 == 0
+      return 'PlingPlangPlong'
+    elsif number % 3 == 0 && number % 5 == 00
+      return 'PlingPlang' 
+    elsif number % 3 == 0 && number % 7 == 00
+      return 'PlingPlong'
+    elsif number % 5 == 0 && number % 7 == 00
+      return 'PlangPlong'
+    elsif number % 3 == 0 
+      return 'Pling'
+    elsif number % 5 == 0 
+      return 'Plang'
+    elsif number % 7 == 0
+      return 'Plong'
+    else
+      return number.to_s
+    end 
+  end
+end 
